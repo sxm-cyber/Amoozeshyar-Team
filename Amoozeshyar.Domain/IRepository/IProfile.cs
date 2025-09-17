8e02108558ed8947406fd9f33a0d8adf837f8ff7
ImanDevback <231675761+ImandevBack@users.noreply.github.com>
2025-09-17 21:44:25 +0330

    Interface,Model

---
+++ Amoozeshyar.Domain/IRepository/IProfile.cs
@@ -0,0 +1,20 @@
+﻿using Amoozeshyar.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amoozeshyar.Domain.IRepository
+{
+    public interface IProfile
+    {
+        Task<Profile> AddAsync(Profile profile);
+
+        Task<Profile?> GetByIdAsync(Guid id);
+
+        Task<Profile?> GetByNameAsync(string fileName);
+
+
+    }
+}
